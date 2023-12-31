e7e3e77386a8b044e46521770e8938c7848fd2be
Alejandro D <aperalta@usp.br>
2023-12-30 22:51:57 -0300

    Install MCIP, BCON, ICON with CMAQ

--- install_CMAQ.sh
+++ install_CMAQ.sh
@@ -19,9 +19,6 @@ git checkout -b my_branch
 
 #> Considerations
 export DIR="/opt/comp_ifort_2021"                   # Only works for AMANAN server with intel compiler
-LIBRARY_PATH="${HOME}/BLDLIB"                       # Find your library folder where I/O API is
-export HOME_IO="${LIBRARY_PATH}/ioapi-3.2"          # Path in which your I/O API was installed
-#export NETCDF_classic=1
 
 #> Building and running in a user-specified directory outside of the repository
  # In the top level of CMAQ_REPO, the bldit_project.csh script will automatically replicate
@@ -32,9 +29,9 @@ sed -i 's: set CMAQ_HOME = /home/username/path: set CMAQ_HOME = ${HOME}/CMAQv5.4
 ./bldit_project.csh
 export CMAQ_HOME="${HOME}/CMAQv5.4"
 
-# We only can compile CMAQ with OpenMPI
+# We only can compile CMAQ with OpenMPI that it is needed include in your ~/.bashrc
 export PATH="$DIR/openmpi/bin:$PATH"
-export LD_LIBRARY_PATH="$BUILD_LIBS/openmpi/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$DIR/openmpi/lib:$LD_LIBRARY_PATH"
 
 which mpifort
 which mpicc
@@ -49,13 +46,11 @@ cd $CMAQ_HOME
 
  #> I/O API, netcdf Library locations used in WRF-CMAQ
  sed -i 's:netcdf_root_intel:${NETCDF}:' config_cmaq.csh
- sed -i 's:ioapi_root_intel:${HOME_IO}:' config_cmaq.csh                 # I/O API passed all tests
-                                                                         # ${HOME_IO} is the path when the I/O API was 
-                                                                         # installed.
+ sed -i 's:ioapi_root_intel:${HOME}/BLDLIB/ioapi-3.2:' config_cmaq.csh   # I/O API location that passed all tests
  sed -i 's:setenv WRF_ARCH #:setenv WRF_ARCH 15 #:' config_cmaq.csh      # Ok according to the AMANAN system
                                                                          # to install WRF-ARW, see details in 
                                                                          # WRF guide installation.
- #sed -i '86,86i\        setenv NETCDF_classic 1' config_cmaq.csh         # adding a new line
+ sed -i '86,86i\        setenv NETCDF_classic 1' config_cmaq.csh         # adding a new line
  #sed -i '87,87i\        setenv WRF_CMAQ 1' config_cmaq.csh               # adding a new line to install WRF_CMAQ
 
  #> I/O API, netCDF, and MPI Library locations used in CMAQ
@@ -80,4 +75,21 @@ sed -i 's:set ICL_MPI   = .:set ICL_MPI   = $MPI_INCL_DIR:' bldit_cctm.csh
 #> Run the bldit_cctm,csh script
 ./bldit_cctm.csh intel |& tee bldit.cctm.log          # See and the end if the executable were created 
 ls BLD_CCTM_v54_intel/*.exe              # if CCTM_v54.exe exists, so you've installed the CMAQ 
+
+#> Install MCIP
+cd $CMAQ_HOME/PREP/mcip/src
+sed -i "51s:/usr/local/apps/netcdf-4.7.3/intel-19.0:$NETCDF:" Makefile
+sed -i "52s:/usr/local/apps/ioapi-3.2_20181011/intel-19.0:${HOME}/BLDLIB/ioapi-3.2:" Makefile
+make |& tee make.mcip.log
+ls mcip.exe
+
+#> Building the BCON executable
+cd $CMAQ_HOME/PREP/bcon/scripts
+./bldit_bcon.csh intel |& tee make_bcon.log
+ls BLD_BCON_v54_intel/*.exe
+
+#> Building the ICON executable
+cd $CMAQ_HOME/PREP/icon/scripts
+./bldit_icon.csh intel |& tee make_icon.log
+ls BLD_ICON_v54_intel/*.exe
 
